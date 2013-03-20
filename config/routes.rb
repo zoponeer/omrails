fe0b055c141ca5903449e8c566b45349aa7b93d6
Xavier Robinson <xavierrobinson@hotmail.com>
2013-03-19 19:10:32 -0500

    add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Omrails::Application.routes.draw do
+
+get 'about' => 'pages#about'
+
+root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
